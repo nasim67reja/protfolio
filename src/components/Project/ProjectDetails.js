fcc3a54edb8f2d79330609a5b21dc988f6c2daa3
nasim67reja <nasimreja67@gmail.com>
2022-10-07 07:18:25 -0700

    almost done Project Deatil Page

--- src/components/Project/ProjectDetails.js
+++ src/components/Project/ProjectDetails.js
@@ -1,51 +1,57 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import careocityFull from "../../Images/projects/carocityFull.png";
 import styles from "./ProjectDet.module.css";
+import { data } from "./projectData";
 
 const ProjectDetails = () => {
   const param = useParams();
+  console.log(param.projectTitle);
+  const ProjectData = data.find((el) => el.title === param.projectTitle);
 
   return (
     <div className={`container ${styles.container}`}>
-      <div>
-        <div>
-          <h3>Harlist</h3>
-          <p>
-            A to do app with a focus on simplicity. Only the features you need
-            to get things done.
-          </p>
-          <div className={styles.tag}>#webdevlopment #frontenddev</div>
-        </div>
-        <div className={styles.imgBox}>
-          <img src={careocityFull} alt="" />
-        </div>
-      </div>
-      <div className={styles.rightColumn}>
-        <h3>The Brief</h3>
-        <p>
-          To do list apps are everywhere, but my biggest issue with them all is
-          how bloated they all are. Social login, user tracking, email
-          notifications. The list goes on. I wanted something really simple to
-          get the job done, so I built it.
-        </p>
-        <h3>Technology Behind</h3>
-        <p></p>
-        <div className={styles.links}>
-          <a
-            className={`${styles.primary}`}
-            href="https://nasim67reja.github.io/CareoCIty-ecommerce/"
-          >
-            View Site
-          </a>
-          <a
-            className={`${styles.primary} ${styles.secondary}`}
-            href="https://github.com/nasim67reja/CareoCIty-ecommerce"
-          >
-            View Code
-          </a>
-        </div>
-      </div>
+      {ProjectData && (
+        <>
+          <div>
+            <div>
+              <h3>{ProjectData.name}</h3>
+              <p>{ProjectData.title}</p>
+              <div className={styles.tag}>{ProjectData.tag}</div>
+              <a href={ProjectData.link}>View Site</a>
+            </div>
+            <div className={styles.imgBox}>
+              <img src={ProjectData.image} alt="website screnshot" />
+            </div>
+          </div>
+          <div className={styles.rightColumn}>
+            <h3>Technology Behind</h3>
+            <ul className={styles.technology}>
+              {ProjectData.technology.map((el, i) => (
+                <li key={i}>{el}</li>
+              ))}
+            </ul>
+            <h3>The Brief</h3>
+            <p>
+              To do list apps are everywhere, but my biggest issue with them all
+              is how bloated they all are. Social login, user tracking, email
+              notifications. The list goes on. I wanted something really simple
+              to get the job done, so I built it.
+            </p>
+
+            <div className={styles.links}>
+              <a className={`${styles.primary}`} href={ProjectData.link}>
+                View Site
+              </a>
+              <a
+                className={`${styles.primary} ${styles.secondary}`}
+                href={ProjectData.linkGithub}
+              >
+                View Code
+              </a>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
---
+++ src/components/Project/projectData.js
@@ -0,0 +1,43 @@
+import careocityFull from "../../Images/projects/carocityFull.png";
+import LoopstudiosFull from "../../Images/projects/LoopstudiosFull.png";
+import EasyBankFull from "../../Images/projects/EasyBankFull.png";
+
+export const data = [
+  {
+    name: "Careocity",
+    title:
+      "Careocity an e-commerce website using react,node,express & tailwindCss",
+    tag: "#webdevlopment #fullstack",
+    link: "https://nasim67reja.github.io/CareoCIty-ecommerce/",
+    linkGithub: "https://github.com/nasim67reja/CareoCIty-ecommerce",
+    image: careocityFull,
+    technology: [
+      "ReactJs",
+      "NodeJs",
+      "ExpressJs",
+      "Mongodb",
+      "TailwindCss",
+      "Redux Toolkit",
+      "React Router",
+      "Axios",
+    ],
+  },
+  {
+    name: "Loopstudios",
+    title: "Responsive landing page using Reactjs & tailwindCss",
+    tag: "#webdevlopment #frontenddev",
+    link: "https://nasim67reja.github.io/Loopstudios.github.io/",
+    linkGithub: "https://github.com/nasim67reja/Loopstudios.github.io",
+    image: LoopstudiosFull,
+    technology: ["ReactJs", "TailwindCss"],
+  },
+  {
+    name: "Easy Bank",
+    title: "Easy Bank - Next generation digital banking",
+    tag: "#webdevlopment #frontenddev",
+    link: "https://nasim67reja.github.io/Easy_Bank.Github.Io/",
+    linkGithub: "https://github.com/nasim67reja/Easy_Bank.Github.Io",
+    image: EasyBankFull,
+    technology: ["Html5", "Css3"],
+  },
+];
