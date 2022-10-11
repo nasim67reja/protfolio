23e9962a0876393b6da8b4cbd6ba7cc5b227d578
nasim67reja <nasimreja67@gmail.com>
2022-10-10 21:22:51 -0700

    Emailjs Package added successfully

--- src/components/Contact/Form.js
+++ src/components/Contact/Form.js
@@ -1,7 +1,11 @@
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 import useMyInput from "../../hook/UseInput";
 import "./Form.css";
 
 const Form = (props) => {
+  const form = useRef();
+
   const inputName = useMyInput((value) => value.trim() !== "");
   const inputNumber = useMyInput((value) => value.trim() !== "");
   const inputLastName = useMyInput((value) => value.trim() !== "");
@@ -17,6 +21,9 @@ const Form = (props) => {
   )
     formIsValid = true;
 
+  const [success, setSuccess] = useState("");
+  const [error, setError] = useState("");
+
   const formSubmissionHandler = (event) => {
     event.preventDefault();
 
@@ -27,6 +34,24 @@ const Form = (props) => {
       !inputEmail.isValid
     )
       return;
+
+    emailjs
+      .sendForm(
+        "service_85av4sx",
+        "template_zplumep",
+        form.current,
+        "cKhVsAvN7VbZ_A_z7"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setSuccess("Form Submitted Successfully ✔");
+        },
+        (error) => {
+          console.log(error.text);
+          setError("form submission failed 😢");
+        }
+      );
 
     inputName.reset();
     inputLastName.reset();
@@ -51,13 +76,16 @@ const Form = (props) => {
     : "form-control";
 
   return (
-    <form onSubmit={formSubmissionHandler}>
+    <form ref={form} onSubmit={formSubmissionHandler} className="form-main">
+      {error && <div className="message">{error}</div>}
+      {success && <div className="message">{success}</div>}
       <div className="control-group">
         <div className={nameInputClasses}>
           <label htmlFor="name">First Name</label>
           <input
             type="text"
             id="name"
+            name="first_name"
             onChange={inputName.valueChangeHandler}
             onBlur={inputName.inputBlurHandler}
             value={inputName.value}
@@ -66,43 +94,47 @@ const Form = (props) => {
             <p className="error-text">Name must not be empty.</p>
           )}
         </div>
-        <div className="form-control">
-          <label htmlFor="number">PHONE NUMBER</label>
+
+        <div className={lastNameInputClasses}>
+          <label htmlFor="lastname">Last Name</label>
           <input
-            type="number"
-            id="number"
-            placeholder="optional"
-            onChange={inputNumber.valueChangeHandler}
-            onBlur={inputNumber.inputBlurHandler}
-            value={inputNumber.value}
+            type="text"
+            id="lastname"
+            name="last_name"
+            onChange={inputLastName.valueChangeHandler}
+            onBlur={inputLastName.inputBlurHandler}
+            value={inputLastName.value}
           />
+          {inputLastName.hasError && (
+            <p className="error-text">Name must not be empty.</p>
+          )}
         </div>
       </div>
+      <div className="form-control">
+        <label htmlFor="number">PHONE NUMBER</label>
+        <input
+          type="number"
+          id="number"
+          placeholder="optional"
+          onChange={inputNumber.valueChangeHandler}
+          onBlur={inputNumber.inputBlurHandler}
+          value={inputNumber.value}
+        />
+      </div>
+
       {/* Email */}
       <div className={inputEmailClasses}>
         <label htmlFor="email">E-Mail Address</label>
         <input
           type="email"
           id="email"
+          name="user_email"
           onChange={inputEmail.valueChangeHandler}
           onBlur={inputEmail.inputBlurHandler}
           value={inputEmail.value}
         />
         {inputEmail.hasError && (
           <p className="error-text">Email must be contain @ ..</p>
-        )}
-      </div>
-      <div className={lastNameInputClasses}>
-        <label htmlFor="lastname">Last Name</label>
-        <input
-          type="text"
-          id="lastname"
-          onChange={inputLastName.valueChangeHandler}
-          onBlur={inputLastName.inputBlurHandler}
-          value={inputLastName.value}
-        />
-        {inputLastName.hasError && (
-          <p className="error-text">Name must not be empty.</p>
         )}
       </div>
 
@@ -120,7 +152,7 @@ const Form = (props) => {
           onChange={inputMessage.valueChangeHandler}
           onBlur={inputMessage.inputBlurHandler}
           value={inputMessage.value}
-          name="contact-message"
+          name="message"
           cols="30"
           rows="10"
         ></textarea>
@@ -139,10 +171,10 @@ const Form = (props) => {
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="feather feather-arrow-right"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="feather feather-arrow-right"
           >
             <line x1="5" y1="12" x2="19" y2="12"></line>
             <polyline points="12 5 19 12 12 19"></polyline>
@@ -154,3 +186,38 @@ const Form = (props) => {
 };
 
 export default Form;
+
+// export const ContactUs = () => {
+
+//   const sendEmail = (e) => {
+//     e.preventDefault();
+
+//     emailjs
+//       .sendForm(
+//         "service_85av4sx",
+//         "template_zplumep",
+//         form.current,
+//         "cKhVsAvN7VbZ_A_z7"
+//       )
+//       .then(
+//         (result) => {
+//           console.log(result.text);
+//         },
+//         (error) => {
+//           console.log(error.text);
+//         }
+//       );
+//   };
+
+//   return (
+//     <form ref={form} onSubmit={sendEmail}>
+//       <label>Name</label>
+//       <input type="text" name="user_name" />
+//       <label>Email</label>
+//       <input type="email" name="user_email" />
+//       <label>Message</label>
+//       <textarea name="message" />
+//       <input type="submit" value="Send" />
+//     </form>
+//   );
+// };
