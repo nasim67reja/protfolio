0f89693fc0f8e5bb8b6035b90cf90772639b3353
nasim67reja <nasimreja67@gmail.com>
2022-10-15 23:07:10 -0700

    Working on About Page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./About.module.css";
 
 const About = () => {
+  const [skillBox, setSkillBox] = useState(true);
+  const [experience, setExperience] = useState(false);
+  const [education, setEducation] = useState(false);
+
   return (
     <div className="container">
       <div className={styles.header}>
@@ -10,80 +14,105 @@ const About = () => {
       </div>
       <ul className={styles.btnlist}>
         <li>
-          <button>Experience</button>
+          <button
+            onClick={() => {
+              setSkillBox(true);
+              setExperience(false);
+              setEducation(false);
+            }}
+          >
+            Professional Skills
+          </button>
         </li>
         <li>
-          <button>Professional Skills</button>
+          <button
+            onClick={() => {
+              setSkillBox(false);
+              setExperience(true);
+              setEducation(false);
+            }}
+          >
+            Experience
+          </button>
         </li>
         <li>
-          <button>Education</button>
+          <button
+            onClick={() => {
+              setSkillBox(false);
+              setExperience(false);
+              setEducation(true);
+            }}
+          >
+            Education
+          </button>
         </li>
       </ul>
+      {experience && (
+        <div className={styles.aboutBox}>
+          <div>
+            <div className={styles.header}>
+              <span>2007 - 2010</span>
+              <h3>Education Quality</h3>
+            </div>
+            <div className={styles.aboutCol}>
+              <div className={styles.line}></div>
+              <div>
+                <div className={styles.textbox}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Excepturi cum enim quam magni, voluptatibus aliquam quia
+                  dolorum blanditiis eos, veniam sint ea reiciendis molestias!
+                  Blanditiis ducimus velit labore repellat ut?
+                </div>
 
-      <div className={styles.aboutBox}>
-        <div>
-          <div className={styles.header}>
-            <span>2007 - 2010</span>
-            <h3>Education Quality</h3>
+                <div className={styles.textbox}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Excepturi cum enim quam magni, voluptatibus aliquam quia
+                  dolorum blanditiis eos, veniam sint ea reiciendis molestias!
+                  Blanditiis ducimus velit labore repellat ut?
+                </div>
+                <div className={styles.textbox}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Excepturi cum enim quam magni, voluptatibus aliquam quia
+                  dolorum blanditiis eos, veniam sint ea reiciendis molestias!
+                  Blanditiis ducimus velit labore repellat ut?
+                </div>
+              </div>
+            </div>
           </div>
-          <div className={styles.aboutCol}>
-            <div className={styles.line}></div>
-            <div>
-              <div className={styles.textbox}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Excepturi cum enim quam magni, voluptatibus aliquam quia dolorum
-                blanditiis eos, veniam sint ea reiciendis molestias! Blanditiis
-                ducimus velit labore repellat ut?
-              </div>
+          {/* <div>2</div> */}
+          {/*  */}
+          <div>
+            <div className={styles.header}>
+              <span>2007 - 2010</span>
+              <h3>Education Quality</h3>
+            </div>
+            <div className={styles.aboutCol}>
+              <div className={styles.line}></div>
+              <div>
+                <div className={styles.textbox}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Excepturi cum enim quam magni, voluptatibus aliquam quia
+                  dolorum blanditiis eos, veniam sint ea reiciendis molestias!
+                  Blanditiis ducimus velit labore repellat ut?
+                </div>
 
-              <div className={styles.textbox}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Excepturi cum enim quam magni, voluptatibus aliquam quia dolorum
-                blanditiis eos, veniam sint ea reiciendis molestias! Blanditiis
-                ducimus velit labore repellat ut?
-              </div>
-              <div className={styles.textbox}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Excepturi cum enim quam magni, voluptatibus aliquam quia dolorum
-                blanditiis eos, veniam sint ea reiciendis molestias! Blanditiis
-                ducimus velit labore repellat ut?
+                <div className={styles.textbox}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Excepturi cum enim quam magni, voluptatibus aliquam quia
+                  dolorum blanditiis eos, veniam sint ea reiciendis molestias!
+                  Blanditiis ducimus velit labore repellat ut?
+                </div>
+                <div className={styles.textbox}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Excepturi cum enim quam magni, voluptatibus aliquam quia
+                  dolorum blanditiis eos, veniam sint ea reiciendis molestias!
+                  Blanditiis ducimus velit labore repellat ut?
+                </div>
               </div>
             </div>
           </div>
         </div>
-        {/* <div>2</div> */}
-        {/*  */}
-        <div>
-          <div className={styles.header}>
-            <span>2007 - 2010</span>
-            <h3>Education Quality</h3>
-          </div>
-          <div className={styles.aboutCol}>
-            <div className={styles.line}></div>
-            <div>
-              <div className={styles.textbox}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Excepturi cum enim quam magni, voluptatibus aliquam quia dolorum
-                blanditiis eos, veniam sint ea reiciendis molestias! Blanditiis
-                ducimus velit labore repellat ut?
-              </div>
-
-              <div className={styles.textbox}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Excepturi cum enim quam magni, voluptatibus aliquam quia dolorum
-                blanditiis eos, veniam sint ea reiciendis molestias! Blanditiis
-                ducimus velit labore repellat ut?
-              </div>
-              <div className={styles.textbox}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Excepturi cum enim quam magni, voluptatibus aliquam quia dolorum
-                blanditiis eos, veniam sint ea reiciendis molestias! Blanditiis
-                ducimus velit labore repellat ut?
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
